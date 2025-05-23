c0cfccf7cd9c42a37494505d5919cbf1fc7676e7
akrupal <162036218+akrupal@users.noreply.github.com>
2025-05-23 13:17:58 +0000

    (WIP) Rate Limiter completed

--- api-server/internal/restapi/middlewares/rate_limiter.go
+++ api-server/internal/restapi/middlewares/rate_limiter.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
+	"encoding/json"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -10,6 +12,7 @@ type Limiter struct {
 	interval time.Duration
 	numReq   int
 	cr       map[int][]time.Time
+	sync.Mutex
 }
 
 func NewRateLimiter(id int, interval time.Duration, numReq int) *Limiter {
@@ -23,10 +26,27 @@ func NewRateLimiter(id int, interval time.Duration, numReq int) *Limiter {
 
 func (rl *Limiter) RateLimiterMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rl.Lock()
+		defer rl.Unlock()
+		now := time.Now()
 		oldReq, exists := rl.cr[rl.id]
 		if !exists {
-			
+			oldReq = []time.Time{}
 		}
+		newReq := []time.Time{}
+
+		for _, e := range oldReq {
+			if now.Sub(e) <= rl.interval {
+				newReq = append(newReq, e)
+			}
+		}
+		newReq = append(newReq, now)
+		if len(newReq) >= rl.numReq {
+			w.WriteHeader(http.StatusTooManyRequests)
+			json.NewEncoder(w).Encode("Too many requests, try after some time!")
+			return
+		}
+		rl.cr[rl.id] = newReq
 		next.ServeHTTP(w, r)
 	})
 }
